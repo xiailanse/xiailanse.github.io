cceee77e295fd6ec6df7d5453ebb0d11e414c05a
xiailanse <1391598154@qq.com>
2016-07-05 23:01:12 +0800

    Site updated: 2016-07-05 23:01:11

--- js/photo.js
+++ js/photo.js
@@ -19,8 +19,8 @@
                 var html, li = "";
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="https://github.com/xiailanse/xiailanse.github.io/blob/master/img/photos/' + data[i] + '?raw=true"></a>' +
+                        '<img lazy-src="https://github.com/xiailanse/xiailanse.github.io/blob/master/img/photos/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
